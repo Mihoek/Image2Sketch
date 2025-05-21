2666a11a68927eb1b30c6fbb776eeab8211827ce
Mihoek <saifeddinefaten06@gmail.com>
2025-05-21 16:43:33 +0200

    adam

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,26 +3,51 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'y'
+project = 'Image2Sketch'
 copyright = '2025, Miheok'
 author = 'Miheok'
-release = 'Image2Sketch'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+# Extensions
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
+]
 
+# Configuration générale
 templates_path = ['_templates']
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# Configuration du thème
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': True,
+    'style_nav_header_background': '#2980B9',
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False,
+}
+
+# Fichiers statiques
 html_static_path = ['_static']
+html_css_files = ['custom.css']
--- docs/source/references.rst
+++ docs/source/references.rst
@@ -4,5 +4,5 @@ Références et ressources
 - Ronneberger, O., Fischer, P., & Brox, T. (2015). U-Net: Convolutional Networks for Biomedical Image Segmentation. arXiv preprint arXiv:1505.04597.
 - Documentation TensorFlow: https://www.tensorflow.org/api_docs
 - Documentation Streamlit: https://docs.streamlit.io/
-- Dataset edge2real: https://www.kaggle.com/datasets/ (Lien spécifique si possible)
+- Dataset edge2real: https://www.kaggle.com/datasets/arsyadmuhammad/edge-2-real-image
 - Code source: https://github.com/Mihoek/Image2Sketch
